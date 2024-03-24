98ed4b325b057c5d54565a9bd45320310f3a36b1
baburali <baburali1910@gmail.com>
2024-03-24 04:03:53 -0700

    add changes

---
+++ app.js
@@ -0,0 +1,97 @@
+// Q.NO-1
+// let message = "hello world"
+// let ul = document.querySelector('ul')
+
+// for(let i = 1; i <= 5; i++){
+//     ul.innerHTML += `<li> ${i} ${message}</li>`
+    
+// }
+
+// Q.NO-2
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// Q.NO-3
+
+// let number = +prompt('Enter Your Table Number');
+// let length = +prompt('How many Times Do you want?');
+
+// for(let number = length; number <= length; number++){
+//     for(let j = 1; j <= length; j++){
+//     console.log(`${number} x ${j} = ${number*j}`);
+//     }
+// }
+
+// Q.NO-4
+
+// let mobileName = ["nokia","samsung","apple","iphone","tecno",];
+
+// let ul = document.querySelector('ul');
+
+// for(let i = 0; i < mobileName.length; i++){
+//     ul.innerHTML += `<li>${mobileName[i]}</li>`
+// }
+
+
+// Q.NO-5
+
+// fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry']
+
+// let ul = document.querySelector('#orderlist')
+// let para = document.querySelector('#orderlist')
+
+// for(let i = 0; i < fruits.length; i++){
+//     ul.innerHTML += `<li> element at index ${i} ${fruits[i]} </li>`
+// }
+
+// Q.NO-6
+// let head = document.querySelector('.head')
+// let paragraph = document.querySelector('.para')
+// let userValue = +prompt('enter your number')
+// head.innerHTML = `enter your ${userValue}`
+
+// let items = prompt ('enter your number?')
+
+// for(let i = 0; i < items ; i++){
+//     paragraph.innerHTML = `<li>your items number ${items}</li>`
+// }
+
+// Q.NO-7
+
+
+// conting====>
+// let h1 = document.querySelector('h1')
+
+// for(let i = 1; i <= 15; i++){
+//     h1.innerHTML += ` ${i} `
+// }
+
+
+// reverse-conting====>
+// let h2 = document.querySelector('h2')
+
+// for(let i = 15; i >=1; i--){
+//     h2.innerHTML += ` ${i} `
+// }
+
+// even====>
+// let h3 = document.querySelector('h3')
+
+// for(let i = 2; i <= 20; i += 2){
+//     h3.innerHTML += ` ${i} `
+// }
+// odd====>
+// let h4 = document.querySelector('h4')
+
+// for(let i = 3; i <= 20; i += 3){
+//     h4.innerHTML += ` ${i} `
+// }
+
+// // series====>
+// let h5 = document.querySelector('h5')
+
+// for(let i = 2; i <= 20; i += 2){
+//     h5.innerHTML += ` ${i + 'k, '} `
+// }
